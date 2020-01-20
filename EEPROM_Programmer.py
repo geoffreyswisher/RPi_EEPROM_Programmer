1e54d5045075fe1804fc07169b6a09c3ad93c6ed
Geoffrey Swisher <geoffreydswisher@gmail.com>
2020-01-20 19:08:29 +0000

    initial commit

---
+++ EEPROM_Programmer.py
@@ -0,0 +1,174 @@
+import wiringpi
+from time import sleep
+
+
+# Pin numbers using wiringPi pin numbering
+# Shift Registers
+SHIFT_DATA = 0
+SHIFT_CLK = 2
+SHIFT_LATCH = 1
+
+# EEPROM
+WRITE_EN = 27
+
+EEPROM_DATA = [3,4,5,6,23,24,26,25]
+
+# Voltage constants
+INPUT = 0
+OUTPUT = 1
+LOW = 0
+HIGH = 1
+MSBFIRST = 1
+LSBFIRST = 0
+
+# Pulse the shift register latch
+
+def pulseLatch():
+
+	wiringpi.digitalWrite(SHIFT_LATCH, HIGH)
+	wiringpi.digitalWrite(SHIFT_LATCH, LOW)
+
+
+# Shifts out the address using the shift registers
+# Attaches output enable bit to the last output
+
+def setAddress(address, outputEnable):
+
+	# shift out greatest 3 bits + output enable (low for true, high for false)
+	wiringpi.shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, ((address >> 8) | (0x00 if outputEnable else 0x80)))
+
+	# shift out the bottom 8 bits
+	wiringpi.shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, (address & 255))
+
+	#pulse latch for output
+	pulseLatch()
+
+
+# Read the data an address from the EEPROM
+
+def readEEPROM(address):
+
+	# setup data pins for input
+	for pin in EEPROM_DATA:
+		wiringpi.pinMode(pin, INPUT)
+
+	setAddress(address, True)
+
+	# return byte
+	data = 0
+	for pin in reversed(EEPROM_DATA):
+		data = (data << 1) + wiringpi.digitalRead(pin)
+
+	return hex(data)
+
+
+# Write to an address on the EEPROM
+
+def writeEEPROM(address, data):
+
+	# setup data pins for output
+	for pin in EEPROM_DATA:
+		wiringpi.pinMode(pin, OUTPUT)
+
+	setAddress(address, False)
+
+	# output the data
+	for pin in EEPROM_DATA:
+		wiringpi.digitalWrite(pin, data & 1)
+		data = data >> 1
+
+	# send the 1000 ns write pulse (not very accurate bc of time module)
+	wiringpi.digitalWrite(WRITE_EN, LOW)
+	sleep(0.0000001)
+	wiringpi.digitalWrite(WRITE_EN, HIGH)
+
+	# delay before next write (just to be sure)
+	sleep(0.1)
+
+
+# prints the first 256 bytes
+
+def print256bytes():
+
+	# print the first 256 bytes in groups of 16
+	for base in range(0,256,16):
+
+		return_list = []
+
+		# the base value - the "0x"
+		return_list.append(hex(base)[2:])
+		return_list.append(": ")
+
+		for offset in range(0, 16):
+
+			# the single byte value - the "0x"
+			return_list.append(readEEPROM(base + offset)[2:])
+			return_list.append(" ")
+
+		return_string = "".join(return_list)
+		print return_string
+
+
+# Prints the entire contents of the EEPROM
+
+def printContents():
+
+	# print the 2048 bytes in groups of 16
+        for base in range(0,2048,16):
+
+                return_list = []
+
+                # the base value - the "0x" 
+                return_list.append(hex(base)[2:])
+                return_list.append(": ")
+
+                for offset in range(0, 16):
+
+                        # the single byte value - the "0x"
+                        return_list.append(readEEPROM(base + offset)[2:])
+                        return_list.append(" ")
+
+                return_string = "".join(return_list)
+                print return_string
+
+
+
+# using the program
+
+def instruct():
+
+	address = 0x9
+
+	print("Reading EEPROM")
+	print(readEEPROM(address))
+
+	print("Writing to EEPROM")
+	writeEEPROM(address, 0x88)
+
+	print("Reading EEPROM")
+	print(readEEPROM(address))
+
+	printContents()
+
+
+# setup board and most pins for use
+
+def setup():
+
+	wiringpi.wiringPiSetup()
+
+	wiringpi.digitalWrite(SHIFT_DATA, LOW)
+	wiringpi.pinMode(SHIFT_DATA, OUTPUT)
+
+	wiringpi.digitalWrite(SHIFT_CLK, LOW)
+	wiringpi.pinMode(SHIFT_CLK, OUTPUT)
+
+	wiringpi.digitalWrite(SHIFT_LATCH, LOW)
+	wiringpi.pinMode(SHIFT_LATCH, OUTPUT)
+
+	wiringpi.digitalWrite(WRITE_EN, HIGH)
+	wiringpi.pinMode(WRITE_EN, OUTPUT)
+
+	instruct()
+
+setup()
